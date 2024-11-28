3a6d167775c907c588337813131182c7ea79c100
Gabriel Rosa <control.brosa07@gmail.com>
2024-11-28 00:42:40 -0300

    [FEAT] Add vote page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center p-6">
-      <h1 className="text-2xl font-bold">
-        Vote na sua opção!
-      </h1>
-      <div className="mt-4"></div>
-    </div>
+    <>
+      <div className="flex min-h-screen flex-col space-y-6 overflow-hidden items-center justify-center p-6">
+        <h1 className="font-bold text-2xl">
+          Essa é a sua primeira vez na Campus Party?
+        </h1>
+        <div className="flex flex-row space-x-6">
+          <Button className="w-[200px]">Sim</Button>
+          <Button className="w-[200px]" variant="outline">
+            Não
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
