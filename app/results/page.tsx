cc501471a759e99dd88b637b19a2635a903317a3
Gabriel Rosa <control.brosa07@gmail.com>
2024-11-28 00:54:00 -0300

    [FEAT] ADD CHART

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,34 +1,9 @@
-"use client";
-// app/results/page.tsx
-import { useEffect, useState } from "react";
+import Chart from "../components/chart";
 
-const ResultsPage = () => {
-  const [results, setResults] = useState({ A: 0, B: 0 });
-
-  useEffect(() => {
-    const fetchResults = async () => {
-      const res = await fetch("/api/vote");
-      const data = await res.json();
-      setResults(data);
-    };
-
-    const interval = setInterval(fetchResults, 1000); // Atualização em tempo real
-    return () => clearInterval(interval);
-  }, []);
-
+export default function ResultsPage() {
   return (
     <>
-      <div className="flex flex-col items-center p-6">
-        <h1 className="text-2xl font-bold">
-          Resultados ao Vivo
-        </h1>
-        <div className="mt-6">
-          <p>Opção A: {results.A} votos</p>
-          <p>Opção B: {results.B} votos</p>
-        </div>
-      </div>
+      <Chart />
     </>
   );
-};
-
-export default ResultsPage;
+}
